abbb9ba893fd1841897e4dae1135d392b8c9a816
yosima050 <byosep050@gmail.com>
2025-02-19 20:46:19 +0700

    Percobaan 3

--- Mahasiswa26.java
+++ Mahasiswa26.java
@@ -36,5 +36,16 @@ public class Mahasiswa26 {
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
         }
     }
-
+    
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
 }
