01fd9b0e3c7d96ebd7af53b2f4f2a106871a9369
yosima050 <byosep050@gmail.com>
2025-02-19 19:32:31 +0700

    Praktikum 2

---
+++ MahasiswaMain26.java
@@ -0,0 +1,14 @@
+public class MahasiswaMain26 {
+    public static void main(String[] args) {
+        Mahasiswa26 mhs1 = new Mahasiswa26();
+        mhs1.nama  = "Muhammad Ali Farhan";
+        mhs1.nim   = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk   = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+    }
+}
